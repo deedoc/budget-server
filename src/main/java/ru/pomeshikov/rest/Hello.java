fca05ad29a7c0f2d483c5d4292989e61dd590986
pomeshikov-s-v <pomeshikov-s-v@JAVA-12.vita-soft.internal>
2013-03-05 13:51:21 +0400

    Init

---
+++ src/main/java/ru/pomeshikov/rest/Hello.java
@@ -0,0 +1,19 @@
+package ru.pomeshikov.rest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import ru.pomeshikov.model.User;
+
+@Controller
+@RequestMapping("/hello")
+public class Hello {
+	@RequestMapping(value="world", method=RequestMethod.GET)
+	public @ResponseBody User world(){
+		User result = new User();
+		result.setName("Gena");
+		return result;
+	}
+}
