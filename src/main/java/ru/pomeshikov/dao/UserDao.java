8888469dd7f3ddb028e6e7769a19aea345f40f64
deedoc <deedoc@deedoc-PC>
2013-03-12 00:03:18 +0400

    unique result

--- src/main/java/ru/pomeshikov/dao/UserDao.java
+++ src/main/java/ru/pomeshikov/dao/UserDao.java
@@ -17,10 +17,10 @@ public class UserDao {
 	}
 	
 	public UserDO findByUkey(String ukey){
-		return (UserDO) sessionFactory.getCurrentSession().createCriteria(UserDO.class).add(Restrictions.eq("ukey", ukey)).list().get(0);
+		return (UserDO) sessionFactory.getCurrentSession().createCriteria(UserDO.class).add(Restrictions.eq("ukey", ukey)).uniqueResult();
 	}
 	
 	public UserDO findByEmailAndPassword(String email, String password){
-		return (UserDO) sessionFactory.getCurrentSession().createCriteria(UserDO.class).add(Restrictions.and(Restrictions.eq("email", email), Restrictions.eq("password", password))).list().get(0);
+		return (UserDO) sessionFactory.getCurrentSession().createCriteria(UserDO.class).add(Restrictions.and(Restrictions.eq("email", email), Restrictions.eq("password", password))).uniqueResult();
 	}
 }
